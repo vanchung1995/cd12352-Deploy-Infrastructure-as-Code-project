c4aedf25a07b6244e02d16d521a650037f70733f
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-04 10:47:15 +0700

    Update run-udagram.sh

--- starter/run-udagram.sh
+++ starter/run-udagram.sh
@@ -1,1 +1,6 @@
 
+aws cloudformation create-stack --stack-name udacity-web \
+    --template-body file://udagram.yml   \
+    --parameters file://udagram-parameters.json  \
+    --capabilities "CAPABILITY_NAMED_IAM"  \
+    --region=us-east-1
