289e11ee4dbc4ab522296bfee1dc867fbe732184
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-04 10:46:32 +0700

    Update run-network.sh

--- starter/run-network.sh
+++ starter/run-network.sh
@@ -1,1 +1,5 @@
-
+aws cloudformation create-stack --stack-name udagram-network \
+    --template-body file://network.yml   \
+    --parameters file://network-parameters.json  \
+    --capabilities "CAPABILITY_NAMED_IAM"  \
+    --region=us-east-1
